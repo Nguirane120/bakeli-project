eea170843b96d6f2f3709f872ad200df9e87286b
Khalifa Fall <falllifa160@gmail.com>
2021-04-21 11:43:59 +0000

    adding teachers

--- src/Composant/App/App.js
+++ src/Composant/App/App.js
@@ -5,7 +5,7 @@ import LandingPage from '../Landing/Landing';
 import SignUpPage from '../Administrateur/InscriptionAdmin/SignupPage';
 import SignInPageApp from '../Apprenant/Connexion/SignInPageApp';
 import SignInPageAdm from '../Administrateur/ConnexionAdmin/SigninPageAdm';
-//import PasswordForgetPage from '../PasswordForget';
+import PasswordForgetPage from '../PasswordForget';
 import HomePageAdmin from '../Administrateur/HomePageAdmin/HomePageAdmin';
 import HomePageApp from '../Apprenant/HomePageApp/HomePageApp';
 //import AccountPage from '../Account';
@@ -13,7 +13,18 @@ import HomePageApp from '../Apprenant/HomePageApp/HomePageApp';
 
 import * as ROUTES from '../../Constant/routes';
 import './App.css';
-//import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+// l'import de khalifa fall
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './stylesheet/styles.css';
+import { Home } from '..Administrateur/Professeur/Home';
+import { Addprofesseur } from '../Administrateur/Professeur/AjoutProfesseur/AjouterProfesseur';
+import { Detailprofesseur } from '..Administrateur/Professeur/DetailProfesseur/DetailProfesseur';
+
+
+import { GlobalProvider } from './context/GlobalState';
 
 const App = () => {
   return (
@@ -29,6 +40,16 @@ const App = () => {
                 <Route path={ROUTES.HOME} component={HomePageApp} />
                 <Route path={ROUTES.HOME_ADM} component={HomePageAdmin} />
             </div>
+
+            // L'import de khalifa fall
+
+            <GlobalProvider>
+               <Switch>
+               <Route path="/" component={Home} exact />
+               <Route path="/add" component={Addprofesseur} exact />
+               <Route path="/edit/:id" component={Detailprofesseur} exact />
+               </Switch>
+            </GlobalProvider>
         </Router>
   );
 }
